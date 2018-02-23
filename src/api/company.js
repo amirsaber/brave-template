951480b8e1b40af24489303e0f8f13397d8cc4d1
AmirSaber Sharifi <amir.s.sharifi@gmail.com>
2018-02-23 13:50:37 -0500

    get all companies

--- src/api/company.js
+++ src/api/company.js
@@ -2,14 +2,20 @@ const Joi = require('joi');
 const uuid = require('uuid');
 
 const createQuery = 'INSERT INTO companies ( id, name, street, city, state ) VALUES ( $1, $2, $3, $4, $5 )';
-const getByIdQuery = 'SELECT * FROM companies WHERE id = $1'
+const getByIdQuery = 'SELECT * FROM companies WHERE id = $1';
+const getAllQuery = 'SELECT * FROM companies';
 
 exports.setup = (server, client) => {
   server.route({
     method: 'GET',
     path: '/api/1/company',
     handler: async function (request, reply) {
-      reply('Hello, world!');
+      try {
+        const rows = (await client.query(getAllQuery)).rows;
+        reply(rows);
+      } catch (e) {
+        reply(e.toString()).code(500)
+      }
     }
   });
 
@@ -22,7 +28,7 @@ exports.setup = (server, client) => {
         const generatedUUID = uuid.v4();
         const result = await client.query(createQuery, [generatedUUID, payload.name, payload.street, payload.city, payload.state]);
         const row = (await client.query(getByIdQuery, [generatedUUID])).rows[0];
-        reply(row);
+        reply(row).code(201);
       } catch (e) {
         reply(e.toString()).code(500)
       }
