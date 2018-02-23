2a3e01f238bc7ea4d0956ba74610d73a9908ab4e
AmirSaber Sharifi <amir.s.sharifi@gmail.com>
2018-02-23 14:49:18 -0500

    stats company by state

--- src/api/stats.js
+++ src/api/stats.js
@@ -1,9 +1,22 @@
+const _ = require('underscore');
+
+const companyByStateQuery = 'SELECT COUNT(*) as count, state FROM companies GROUP BY state';
+
 exports.setup = (server, client) => {
   server.route({
     method: 'GET',
-    path: '/api/1/stats',
+    path: '/api/1/stats/companyByState',
     handler: async function (request, reply) {
-      reply('Hello, world!');
+      try {
+        const rows = (await client.query(companyByStateQuery)).rows;
+        const result = {};
+        _.each(rows, function (row) {
+          result[row.state] = row.count
+        });
+        reply(result);
+      } catch (e) {
+        reply(e.toString()).code(500)
+      }
     }
   });
 }
