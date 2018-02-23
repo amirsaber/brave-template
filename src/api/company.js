4e48f0f198d11e2e3d7ddd431af90107f87697ac
AmirSaber Sharifi <amir.s.sharifi@gmail.com>
2018-02-23 14:08:54 -0500

    update

--- src/api/company.js
+++ src/api/company.js
@@ -4,6 +4,7 @@ const uuid = require('uuid');
 const createQuery = 'INSERT INTO companies ( id, name, street, city, state ) VALUES ( $1, $2, $3, $4, $5 )';
 const getByIdQuery = 'SELECT * FROM companies WHERE id = $1';
 const getAllQuery = 'SELECT * FROM companies';
+const updateById = 'UPDATE companies SET (name, street, city, state) = ($2, $3, $4, $5) WHERE id = $1';
 
 exports.setup = (server, client) => {
   server.route({
@@ -61,6 +62,41 @@ exports.setup = (server, client) => {
     config: {
       validate: {
         payload: {
+          name: Joi.string().required(),
+          street: Joi.string().optional(),
+          city: Joi.string().optional(),
+          state: Joi.string().required()
+        }
+      }
+    }
+  });
+
+  server.route({
+    method: 'PUT',
+    path: '/api/1/company/{guid}',
+    handler: async function (request, reply) {
+      const payload = request.payload;
+      try {
+        const guid = request.params.guid;
+        let row = (await client.query(getByIdQuery, [guid])).rows[0];
+        if (!row) {
+          reply().code(404);
+        } else {
+          const result = await client.query(updateById, [guid, payload.name, payload.street, payload.city, payload.state]);
+          row = (await client.query(getByIdQuery, [guid])).rows[0];
+          reply(row).code(200);
+        }
+      } catch (e) {
+        reply(e.toString()).code(500)
+      }
+    },
+    config: {
+      validate: {
+        params: {
+          guid: Joi.string().uuid()
+        },
+        payload: {
+          id: Joi.forbidden(),
           name: Joi.string().required(),
           street: Joi.string().optional(),
           city: Joi.string().optional(),
